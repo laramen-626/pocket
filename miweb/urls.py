f290a09a999eff4dc23017dc44d5efe04d76dca4
Laramen <laramen.626@gmail.com>
2025-06-15 23:03:00 -0400

    urls.py

--- miweb/urls.py
+++ miweb/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pagina.urls')),
+    path('dreamjournal/', views.dreamjournal, name='dreamjournal'),
     ]
